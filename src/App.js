2b9167848cc25ddb05825c8d1851b8ffbae1b161
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-10 14:37:03 +0100

    fetching the data using the useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+
+  useEffect(function () {
+    async function convert(from, to, amount) {
+      const res = await fetch(
+        `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${from}&to=${to}`
+      );
+      const data = await res.json();
+
+      const convertedAmount = (amount * data.rates[to]).toFixed(2);
+      alert(`${amount} ${from} = ${convertedAmount} ${to}`);
+    }
+
+    convert("EUR", "USD", 10);
+  }, []);
   return (
     <div>
       <input type="text" />
