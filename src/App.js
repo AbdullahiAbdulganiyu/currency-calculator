897a14d801d35948584d363d4ee9cacda8ba6da5
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-10 15:32:03 +0100

    fixing the fetch url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
     function () {
       async function convert() {
         const res = await fetch(
-          `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${from}&to=${to}`
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
         );
         const data = await res.json();
 
